4a31326590583229af152eb1c825b8150fb1500c
Felice Forby <feliceforby@gmail.com>
2019-03-23 04:16:14 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,23 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+  
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+  
+  // Set the initial state, as defined in the reducer
+  dispatch({type: '@@INIT'});
+  
+  function getState() {
+    return state;
+  }
+  
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
